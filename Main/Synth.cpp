fb06423b499be445eccd5b00c2c9f21abf57db63
Michael Zarimis <michael@misadigital.com>
2011-08-10 13:09:18 +0800

    Undid code that restarts note envelope automatically if switched off. It's a bad idea.

--- Main/Synth.cpp
+++ Main/Synth.cpp
@@ -2490,10 +2490,10 @@ void Synth::sendStopSound(unsigned char str, unsigned char btn)
 			if(string_note[str] != -1)
 				midi.sendNoteOff(SYNTH, current_setting.voices[str].at(i).channel, string_note[str], 0);
 			midi.sendSoundOff(SYNTH, current_setting.voices[str].at(i).channel);
-			setMuteChannelVolume(str, i); //note envelope will auto restart
-			midi.sendNoteOn(SYNTH, current_setting.voices[str].at(i).channel, string_note[str], current_setting.voices[str].at(i).velocity); //note envelope will auto restart
+//			setMuteChannelVolume(str, i); //note envelope will auto restart
+//			midi.sendNoteOn(SYNTH, current_setting.voices[str].at(i).channel, string_note[str], current_setting.voices[str].at(i).velocity); //note envelope will auto restart
 		}
-//		string_note[str] = -1;
+		string_note[str] = -1;
 	}
 }
 
